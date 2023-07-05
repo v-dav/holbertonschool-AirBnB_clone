271f1b24127e785c48dfce985e22c40bf3c9ffa2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-05 16:48:42 +0200

    try to find the error

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,10 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        # if kwargs and kwargs != {}:
-        #     for key, value in kwargs.items():
-        #         if key == 'created_at' or key == 'updated_at':
-        #             value = datetime.strptime(
-        #                 value, "%Y-%m-%dT%H:%M:%S.%f")
-        #         if key != '__class__':
-        #             self.__dict__[key] = value
-
     def __str__(self):
         """representation of the instance"""
         return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute"""
