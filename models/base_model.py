4958269e811e850b804168e29d26d3cfae8023d0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-05 16:35:13 +0200

    try to fix the code

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
-"""is the base class of all our models. It contains common elements:
-attributes: id, created_at and updated_at
-methods: save() and to_json()"""
+"""The base class of all our models"""
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    """Defines all common attributes/methods for other classes"""
 
-        else:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
-                    self.__dict__[key] = value
+    def __init__(self):
+        """The class constructor method"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        # if kwargs and kwargs != {}:
+        #     for key, value in kwargs.items():
+        #         if key == 'created_at' or key == 'updated_at':
+        #             value = datetime.strptime(
+        #                 value, "%Y-%m-%dT%H:%M:%S.%f")
+        #         if key != '__class__':
+        #             self.__dict__[key] = value
 
     def __str__(self):
         """representation of the instance"""
@@ -34,9 +34,25 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values"""
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
+
+
+# my_model = BaseModel()
+# my_model.name = "My First Model"
+# my_model.my_number = 89
+# print(my_model)
+# my_model.save()
+# print(my_model)
+# my_model_json = my_model.to_dict()
+# print(my_model_json)
+# print("JSON of my_model:")
+# for key in my_model_json.keys():
+#     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
+# my_model_json[key]))
