0b74c879be11b93fa8fe2a82bce89713885a808a
djamalMhmd <djamaldine-mohamed@live.fr>
2023-07-05 15:06:05 +0200

    Write BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,39 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
 """is the base class of all our models. It contains common elements:
 attributes: id, created_at and updated_at
 methods: save() and to_json()"""
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.created_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key != '__class__':
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """"""
+        return "[{}] ({}) {}".format(self.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """updates the public instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['created_at'] = self.created_at.isoformat()
+        return my_dict
