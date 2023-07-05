63186843e0afc4bbe772fdd2eb430ca696ef8a55
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-05 17:26:30 +0200

    update class and script docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -10,41 +10,35 @@ class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
     def __init__(self):
-        """The class constructor method"""
+        """The class constructor method for the class instance"""
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """representation of the instance"""
+        """Public instance method for human readable
+        representation of the instance.
+
+        Return: a string"""
+
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute"""
+        """Public instance method that updates
+        the public instance attribute."""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
+        """Public instance method that updates and returns
+        a dictionary containing all keys/values of __dict__ of the instance.
+
+        Returns: a dictionary."""
 
         my_dict = self.__dict__
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
-
-
-# my_model = BaseModel()
-# my_model.name = "My First Model"
-# my_model.my_number = 89
-# print(my_model)
-# my_model.save()
-# print(my_model)
-# my_model_json = my_model.to_dict()
-# print(my_model_json)
-# print("JSON of my_model:")
-# for key in my_model_json.keys():
-#     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-# my_model_json[key]))
