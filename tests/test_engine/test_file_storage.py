46e1b1b31be9e9fd543400f4b7c55bb931be69e2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-06 17:32:10 +0200

    update tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,47 +1,37 @@
-#!/usr/bin/python3
-from models.engine.file_storage import FileStorage
-from models import storage
-from models.base_model import BaseModel
-import pycodestyle
 import unittest
-import json
-import os
-
-""" Module for FileStorage unit tests """
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    """ contains all unit tests """
 
-    def test__file_path(self):
-        """ tests the presence of file.json name """
-        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
-
-    def test_objects(self):
-        """ tests that __objects is a dictionary """
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-
-    def test_reload(self):
-        """ test that an exception is raised """
-        with self.assertRaises(TypeError):
-            storage.reload(None)
+    def setUp(self):
+        self.file_storage = FileStorage()
+        self.base_model = BaseModel()
 
     def test_all(self):
-        """ tests 'all' method """
-        self.assertEqual(FileStorage._FileStorage__objects, storage.all())
+        initial_objects = self.file_storage.all()
+        self.base_model.save()
+        after_save_objects = self.file_storage.all()
+        self.assertEqual(len(initial_objects), len(after_save_objects))
+
+    def test_new(self):
+        initial_objects = self.file_storage.all()
+        self.file_storage.new(self.base_model)
+        after_new_objects = self.file_storage.all()
+        self.assertEqual(len(initial_objects), len(after_new_objects))
 
     def test_save(self):
-        """ tests the 'save' method """
-        if os.path.exists(FileStorage._FileStorage__file_path):
-            with open(FileStorage._FileStorage__file_path, 'r') as f:
-                self.assertEqual(dict, type(json.load(f)))
+        initial_objects = self.file_storage.all()
+        self.file_storage.save()
+        after_save_objects = self.file_storage.all()
+        self.assertEqual(len(initial_objects), len(after_save_objects))
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pycodestyle.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_reload(self):
+        initial_objects = self.file_storage.all()
+        self.file_storage.reload()
+        after_reload_objects = self.file_storage.all()
+        self.assertEqual(len(initial_objects), len(after_reload_objects))
 
 
 if __name__ == '__main__':
