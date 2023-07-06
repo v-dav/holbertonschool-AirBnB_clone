e7cd34bc20eb252563d514030766a12f1cae1754
djamalMhmd <djamaldine-mohamed@live.fr>
2023-07-06 10:07:10 +0200

    update file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+def all(self):
+    return self.__objects
+
+def new(self, obj):
+    pass
+
+def save(self):
+    pass
+
+def reload(self):
+    pass
