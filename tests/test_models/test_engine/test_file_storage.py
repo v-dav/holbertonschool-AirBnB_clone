33f126f047727647c011ba913903c7709da1b193
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-06 17:49:59 +0200

    write tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,7 @@
 from models.engine.file_storage import FileStorage
 from models import storage
 from models.base_model import BaseModel
-import pep8
+import pycodestyle
 import unittest
 import json
 
@@ -36,7 +36,7 @@ class TestFileStorage(unittest.TestCase):
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
+        pep8style = pycodestyle.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
