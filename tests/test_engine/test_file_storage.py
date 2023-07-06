26a2887c18b38d05210f82c75a6cc1f9105fc493
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-06 17:18:51 +0200

    update tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -2,9 +2,9 @@
 from models.engine.file_storage import FileStorage
 from models import storage
 from models.base_model import BaseModel
+import pycodestyle
 import unittest
 import json
-import os
 
 """ Module for FileStorage unit tests """
 
@@ -31,9 +31,15 @@ class TestFileStorage(unittest.TestCase):
 
     def test_save(self):
         """ tests the 'save' method """
-        if os.path.exists(FileStorage._FileStorage__file_path):
-            with open(FileStorage._FileStorage__file_path, 'r') as f:
-                self.assertEqual(dict, type(json.load(f)))
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            self.assertEqual(dict, type(json.load(f)))
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 
 if __name__ == '__main__':
