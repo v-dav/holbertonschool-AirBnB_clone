39d73ff2657cf97c90ed9134ac4e7f6dc34bb789
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-07 09:30:51 +0200

    fix the emptyline cmd and improve docstrings

--- console.py
+++ console.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-"""Commande interpreter"""
+"""Commande line interpreter for the AirBnb Console"""
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class representing the command interpreter"""
+    """Class representing the command interpreter
+    """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Exit the program"""
-        return True
-  
-    def do_EOF(self, arg):
-        """End of the file"""
+        """Exit the program with quit command
+        """
         return True
 
-    def emptyline(self, arg):
-        """Empty line"""
+    def do_EOF(self, arg):
+        """End of the file, a method allowing
+        to make a clean exit of the interpreter with Ctrl-D
+        """
+        return True
+
+    def emptyline(self):
+        """Empty line
+        """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
