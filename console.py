4e43dc2e99ddedff7bef396644ce1a5bcba5ca11
djamalMhmd <djamaldine-mohamed@live.fr>
2023-07-07 02:46:00 +0200

    Write a program called console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class representing the command interpreter"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """End of the file"""
+        return True
+    
+    def emptyline(self, arg):
+        """Empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
