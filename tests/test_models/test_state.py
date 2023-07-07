52f6b2e8acb093f0808f8cb395256baa80607652
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-07 17:55:28 +0200

    try to fix

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,8 +1,18 @@
+#!/usr/bin/python3
+"""Unittest for the state module"""
+
+
 import unittest
 from models.state import State
 
 
 class TestState(unittest.TestCase):
-    def test_class(self):
-        classe = State()
-        self.assertEqual(classe.name, "")
+    """Tests"""
+    def test_default_attributes(self):
+        """Tests"""
+        state = State()
+        self.assertEqual(state.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
