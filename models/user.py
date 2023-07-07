99a4cd9fe7a9c10e62395416cc06a0944a37153f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-07-07 15:55:24 +0200

    create class user for the program

--- models/user.py
+++ models/user.py
@@ -2,27 +2,19 @@
 
 """Class User"""
 
-from base_model import BaseModel
+from models.base_model import BaseModel
+
 
 class User(BaseModel):
-    """Class representing user"""
+    """Class representing User.
+
+    Attributes:
+        email (str): user email
+        password (str): user password
+        first_name (str): user's first name
+        last_name (str): user's last name
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-
-from engine.file_storage import FileStorage
-from __init__ import storage
-
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new User --")
-my_user = User()
-my_user.first_name = "Betty"
-my_user.last_name = "Bar"
-print(my_user)
